0fe6d21804dbee0958fa50580709e58f7363e30d
DevAzeredo <developer.azeredo@gmail.com>
2024-01-06 13:52:17 -0300

    feito botoes pra salvar e criar novas tasks

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../shared/interfaces/task.interface';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-task',
@@ -10,67 +11,64 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
-export class TaskComponent implements OnInit{
+export class TaskComponent implements OnInit {
+  private apiUrl = 'http://localhost:3000/tasks';
   changesMade: boolean = false;
-tasks: Task[]=[];
+  tasks: Task[] = [];
+  newTasks: Task[] = [];
+  tasksLoaded: boolean = false;
+  constructor( private http: HttpClient) {
+  }
 
-tasksLoaded :boolean = false;
-constructor(private http: HttpClient) {}
+  createTasks(tasks: Task[]): Observable<Task[]> {
+    return this.http.post<Task[]>(`${this.apiUrl}`, tasks);
+  }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}`);
+  }
+
   ngOnInit() {
     this.getTasksFromBackend();
   }
+
   getTasksFromBackend() {
-
-
-    this.tasks =[ {
-      "id": "65872e95a1b2e2026ea3d436",
-      "title": "ALTEREI O NOME DESSA task",
-      "description": "teste descricao 1",
-      "completed": false
-  }, {
-    "id": "65872e95a1b2e2026ea3d436",
-    "title": "ALTEREI O NOME DESSA task",
-    "description": "teste descricao 1",
-    "completed": false
-}, {
-  "id": "65872e95a1b2e2026ea3d436",
-  "title": "ALTEREI O NOME DESSA task",
-  "description": "teste descricao 1",
-  "completed": false
-}, {
-  "id": "65872e95a1b2e2026ea3d436",
-  "title": "ALTEREI O NOME DESSA task",
-  "description": "teste descricao 1",
-  "completed": false
-}, {
-  "id": "65872e95a1b2e2026ea3d436",
-  "title": "ALTEREI O NOME DESSA task",
-  "description": "teste descricao 1",
-  "completed": false
-}, {
-  "id": "65872e95a1b2e2026ea3d436",
-  "title": "ALTEREI O NOME DESSA task",
-  "description": "teste descricao 1",
-  "completed": false
-}, {
-  "id": "65872e95a1b2e2026ea3d436",
-  "title": "ALTEREI O NOME DESSA task",
-  "description": "teste descricao 1",
-  "completed": false
-},];
-    /*this.http.get<Task[]>('http://localhost:3000/tasks').subscribe({
-      next: (tasks) => {
-        this.tasks = tasks;
-        console.log(tasks);
-        
-      },
-      error: (error) => {
-        console.error('Erro ao obter tarefas do backend:', error);
-      },
-    });*/
-    this.tasksLoaded = true;
+    this.getTasks().subscribe(
+      {
+        next: (tasks: Task[]) => this.tasks = tasks,
+        error: (error: any) => console.error('Erro ao carregar tarefas:', error),
+      }
+    );
   }
-saveChanges() {
-throw new Error('Method not implemented.');
-}
+
+  saveChanges() {
+
+  }
+  createNewTask() {
+    const newTask: Task = {
+      description: "",
+      completed: false,
+      id: "",
+      title: "",
+      confirmDelete: false,
+    };
+    this.newTasks.push(newTask);
+  }
+  deleteTask(task: Task) {
+    const index = this.newTasks.indexOf(task);
+    if (index !== -1) {
+      this.newTasks.splice(index, 1);
+    }
+  }
+  toggleDeleteConfirmation(task: Task) {
+    if (task.intentionDelete) {
+      task.confirmDelete = !task.confirmDelete;
+    }
+    if (task.confirmDelete) {
+      console.log('1');
+      this.deleteTask(task);
+    }
+    task.intentionDelete = !task.intentionDelete;
+  }
+
 }
